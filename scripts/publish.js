df89b5f5039950d1bc0bee4aa57adabb1da7aa35
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-02-13 15:39:11 -0500

    fix: Use file to store json.

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import { $ } from "zx";
 
-const outputs = JSON.parse(process.argv[2]);
-
+const outputs = JSON.parse(await readFile(process.argv[2], "utf-8"));
 for (const key in outputs) {
   const value = outputs[key];
   const match = key.match(/^(.*\/.*)--release_created$/);
