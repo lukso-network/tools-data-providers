7451892ddba75f1f1686157497bfd38a950a5a4f
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-04-05 11:15:56 -0400

    fix: Apply some upgrades and lint fixes. Remove eslint

--- configs/jest-presets/jest/browser/jest-setup.js
+++ configs/jest-presets/jest/browser/jest-setup.js
@@ -7,31 +7,27 @@ require("web-streams-polyfill");
 config({ path: "../../.env.test" });
 
 Object.defineProperty(globalThis, "crypto", {
-	value: crypto,
+  value: crypto,
 });
 Object.defineProperty(globalThis, "Uint32Array", {
-	value: Uint32Array,
+  value: Uint32Array,
 });
 Object.defineProperty(globalThis, "Uint8Array", {
-	value: Uint8Array,
+  value: Uint8Array,
 });
 Object.defineProperty(globalThis, "ArrayBuffer", {
-	value: ArrayBuffer,
+  value: ArrayBuffer,
 });
 
-// eslint-disable-next-line unicorn/prefer-module
 Object.defineProperty(globalThis, "ArrayBuffer", {
-	value: ArrayBuffer,
+  value: ArrayBuffer,
 });
-// eslint-disable-next-line unicorn/prefer-module
 Object.defineProperty(globalThis, "TextDecoder", {
-	value: TextDecoder,
+  value: TextDecoder,
 });
-// eslint-disable-next-line unicorn/prefer-module
 Object.defineProperty(globalThis, "TextEncoder", {
-	value: TextEncoder,
+  value: TextEncoder,
 });
-// eslint-disable-next-line unicorn/prefer-module
 Object.defineProperty(globalThis, "ResizeObserver", {
-	value: ResizeObserver,
+  value: ResizeObserver,
 });
--- packages/data-provider-queries/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-export default {
-  root: true,
-  extends: ["lukso"],
-  rules: {
-    "sonarjs/no-duplicate-string": "off",
-  },
-};
